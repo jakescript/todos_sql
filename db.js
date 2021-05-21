c21f8910261a17dcc2580d8d63e8f440d7a978c9
Jacob Goldstein <by.jakeg@gmail.com>
2021-05-21 15:09:17 -0400

    finished

--- db.js
+++ db.js
@@ -1,24 +1,27 @@
 const Sequelize = require('sequelize')
 
-const conn = new Sequelize('todos', 'jake', 'captincarl69', {
-    host: 'localhost',
-    dialect: 'postgres'
-  });
+const conn = new Sequelize('postgres://localhost/todos', {logging: false});
 
 const Todo = conn.define('todo', {
     name: Sequelize.DataTypes.STRING,
     completed: {type: Sequelize.DataTypes.BOOLEAN, defaultValue: false}
 })
 
+const todos = [
+    { name: "finish demo" },
+    { name: "eat something" },
+    { name: "have coffee", completed: true }
+]
 
 const seed = async () => {
-    conn.sync({force: true})
-    await Todo.create({name: "finish demo"})
+    await conn.sync({force: true})
+    await Promise.all(todos.map(todo => Todo.create(todo)))
 }
 
 module.exports = {
     conn,
     models: {
         Todo
-    }
+    },
+    seed
 }
--- server.js
+++ server.js
@@ -1,13 +1,74 @@
 const express = require('express')
-const {models: {Todo}} = require('./db')
+const path = require('path')
+const methodOverride = require('method-override')
+const {models: {Todo}, seed} = require('./db')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('hello')
+app.use(express.urlencoded({extended: true}))
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+    const todos = await Todo.findAll()
+    res.send(`
+    <html>
+    <head>
+        <link rel='stylesheet' href='/public/styles.css'>
+    </head>
+    <body>
+        <div>
+            <form method='POST' action='/todos'>
+                <input type='text' name='name'/>
+                <button> create </button>
+            </form>
+
+            <ul>
+                ${todos.map(todo =>`
+                    <li class=${todo.completed ? 'completed' : ''}>
+                        ${todo.name}
+                        <form method='POST' action='/todos/${todo.id}?_method=DELETE'>
+                            <button>X</button>
+                        </form>
+
+                        <form method='POST' action='/todos/${todo.id}?_method=PUT'>
+                            <button>${todo.completed ? 'Unfinished' : 'Done'}</button>
+                        </form>
+                    </li>
+                `).join('')}
+            </ul>
+        </div>
+    </body>
+    </html>
+    `)
 })
 
-app.get('/todo', async (req, res) => {
+app.put('/todos/:id', async (req, res) => {
+    const todo = await Todo.findByPk(req.params.id)
+    todo.completed = !todo.completed
+    await todo.save()
+    res.redirect('/')
+})
+
+app.delete('/todos/:id', async (req, res) => {
+    const todo = await Todo.findByPk(req.params.id)
+    await todo.destroy()
+    res.redirect('/')
+})
+
+app.get('/todos', async (req, res) => {
     res.send(await Todo.findAll())
 })
 
-app.listen(3000, () => console.log('listening'))
+app.post('/todos', async (req, res) => {
+    await Todo.create(req.body)
+    res.redirect('/')
+})
+
+const run = async () => {
+    await seed()
+    console.log("Seeded")
+    app.listen(3000, () => console.log('listening'))
+}
+
+run()
+
